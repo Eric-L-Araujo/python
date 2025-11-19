cd4fa6121d614b6a54f21194d23ca6b240c0f06b
Eric Araujo <ericaraujo77@hotmail.com>
2025-11-19 14:45:30 -0300

    Create 001.py

---
+++ DIO/001.py
@@ -0,0 +1,9 @@
+
+nome = input("Insira seu nome, querido: ")
+funcao = input("Insira sua função, querido: ")
+
+print(f"Nome do querido: {nome}")
+print(f"função do querido: {funcao}")
+
+print("Processando . . .")
+print("Fim")
